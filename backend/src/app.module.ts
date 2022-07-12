20dedf964ef0d97efb68f8fb3c2f3c0102e486c4
hyoseung <hseung578@gmail.com>
2022-07-12 15:00:44 +0900

    cors

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -38,6 +38,10 @@ import { ReviewModule } from './apis/review/review.module';
       driver: ApolloDriver,
       autoSchemaFile: 'src/commons/graphql/schema.gql',
       context: ({ req, res }) => ({ req, res }),
+      cors: {
+        origin: 'http://localhost:3000',
+        credentials: true,
+      },
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
