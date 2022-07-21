6009bb18448b51eb2d5eae3370502a6e50af2503
hyoseung <hseung578@gmail.com>
2022-07-21 21:27:59 +0900

    update fetchLoginUser

--- backend/src/apis/tasks/tasks.service.ts
+++ backend/src/apis/tasks/tasks.service.ts
@@ -1,21 +1,27 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { getRepository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { getRepository, Repository } from 'typeorm';
+import { Car } from '../cars/entities/car.entity';
 import { Reservation } from '../reservations/entities/reservation.entity';
 
 @Injectable()
 export class TasksService {
+  constructor(
+    @InjectRepository(Car)
+    private readonly carRepository: Repository<Car>,
+  ) {}
   private readonly logger = new Logger(TasksService.name);
 
   @Cron(CronExpression.EVERY_5_SECONDS)
   async runEveryMinute() {
-    // console.log('every 5 seconds');
+    console.log('every 5 seconds');
     // const now = new Date();
-    // const reservation = await getRepository(Reservation)
-    //   .createQueryBuilder('reservation')
-    //   .where('reservation.status = :using', { using: 'USING' })
-    //   .andWhere('reservation.endTime < :now', { now })
-    //   .getMany();
-    // console.log(reservation);
+    // const car = await this.carRepository.find({
+    //   where: { isVaild: false },
+    // });
+    // await Promise.all(
+    //   car.map()
+    // )
   }
 }
