0bfc0c4a8e065b0c6994988f8a1f9f3e40ab0f9e
hyoseung <hseung578@gmail.com>
2022-07-22 22:39:19 +0900

    scheduler&contract

--- backend/src/apis/cars/car.resolver.ts
+++ backend/src/apis/cars/car.resolver.ts
@@ -1,6 +1,4 @@
-import { UseGuards } from '@nestjs/common';
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { GqlAuthAccessGuard } from 'src/commons/auth/gql-auth.guard';
 import { CarService } from './car.service';
 import { CreateCarInput } from './dto/createCar.input';
 import { PopularCarOutput } from './dto/popularCar.output';
@@ -27,7 +25,7 @@ export class CarResolver {
     })
     page: number,
     @Args({ name: 'carLocationId', description: '차량존 UUID' })
-    carLocationId: string, //
+    carLocationId: string,
   ): Promise<Car[]> {
     return this.carService.findAll({ carLocationId, page });
   }
@@ -40,8 +38,16 @@ export class CarResolver {
   }
 
   @Query(() => [Car], { description: '차량 리스트 조회 (관리자)' })
-  fetchCarsWithDeleted(): Promise<Car[]> {
-    return this.carService.findAllWithDeleted();
+  fetchCarsWithDeleted(
+    @Args({
+      name: 'page',
+      type: () => Int,
+      nullable: true,
+      description: '페이지 넘버',
+    })
+    page: number,
+  ): Promise<Car[]> {
+    return this.carService.findAllWithDeleted({ page });
   }
 
   @Query(() => [PopularCarOutput], { description: '인기 차량 조회' })
@@ -57,28 +63,37 @@ export class CarResolver {
     return this.carService.create({ createCarInput });
   }
 
-  @UseGuards(GqlAuthAccessGuard)
   @Mutation(() => String, { description: '계약 중지' })
-  async deleteCar(
+  async stopContract(
     @Args({ name: 'carId', description: '차량 UUID' }) carId: string,
   ): Promise<string> {
-    const result = await this.carService.delete({ carId });
+    const result = await this.carService.updateIsValid({
+      carId,
+      isValid: false,
+    });
     if (result) return '계약이 중지되었습니다';
     else return '계약 중지를 실패하였습니다';
   }
 
   @Mutation(() => String, { description: '계약 재시작' })
-  async restoreCar(@Args('carId') carId: string): Promise<string> {
-    const result = await this.carService.restore({ carId });
+  async restartContract(
+    @Args({ name: 'carId', description: '차량 UUID' }) carId: string,
+  ): Promise<string> {
+    const result = await this.carService.updateIsValid({
+      carId,
+      isValid: true,
+    });
     if (result) return '계약이 재시작되었습니다';
     else return '계약 재시작을 실패하였습니다';
   }
 
   @Mutation(() => String, { description: '기간 재계약' })
   async refreshContract(
-    @Args('contractStart') contractStart: Date,
-    @Args('contractEnd') contractEnd: Date,
-    @Args('carId') carId: string,
+    @Args({ name: 'contractStart', description: '계약시작 시간' })
+    contractStart: Date,
+    @Args({ name: 'contractEnd', description: '계약종료 시간' })
+    contractEnd: Date,
+    @Args({ name: 'carId', description: '차량 UUID' }) carId: string,
   ): Promise<string> {
     const result = await this.carService.updateContract({
       carId,
