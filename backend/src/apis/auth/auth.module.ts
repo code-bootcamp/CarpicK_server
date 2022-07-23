1f337f18818a099b6e0642be805fdcae4d013aad
hyoseung <hseung578@gmail.com>
2022-07-23 22:18:33 +0900

    socialLogin

--- backend/src/apis/auth/auth.module.ts
+++ backend/src/apis/auth/auth.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtRefreshStrategy } from 'src/commons/auth/jwt-refresh.strategy';
-import { JwtGoogleStrategy } from 'src/commons/auth/jwt-social-google.strategy';
 import { AdministratorService } from '../administrator/administrator.service';
 import { User } from '../users/entities/user.entity';
 import { Administrator } from '../administrator/entities/administrator.entity';
 import { UserService } from '../users/user.service';
-import { AuthController } from './auth.controller';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { ImageStart } from '../imagesStart/entities/imageStart.entity';
@@ -28,15 +26,11 @@ import { Reservation } from '../reservations/entities/reservation.entity';
     ]),
   ],
   providers: [
-    JwtGoogleStrategy,
     JwtRefreshStrategy,
     AuthResolver,
     AuthService,
     UserService,
     AdministratorService,
-  ],
-  controllers: [
-    AuthController, //
   ],
 })
 export class AuthModule {}
