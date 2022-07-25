507b273db85e57c3369c0f5d616943e5f25abf3d
hyoseung <hseung578@gmail.com>
2022-07-25 15:38:40 +0900

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { graphqlUploadExpress } from 'graphql-upload';
 import { AppModule } from './app.module';
-// import { AppLocalModule } from './appLocal.module';
 import { HttpExcptionFilter } from './commons/filter/http-exception.filter';
 
 async function bootstrap(): Promise<void> {
